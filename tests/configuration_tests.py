cdd1d23e111d7d6ebcab807c3853a27c35f32486
Melle Dijkstra <melle210202@gmail.com>
2018-04-11 21:47:24 +0200

    Added travis configuration & changed tests files

--- tests/configuration_tests.py
+++ tests/configuration_tests.py
@@ -13,7 +13,8 @@ class TestApplicationConfiguration(unittest.TestCase):
         self.test_config_file_is_present()
         # load configuration for tests
         self.config = RawConfigParser()
-        self.config.read_file(open(self.configfile))
+        with open(self.configfile, 'r') as file:
+            self.config.read_file(file)
 
     def test_config_file_is_present(self):
         self.assertEqual(os.path.isfile(self.configfile), True, "Config file not present! you should create one "
@@ -22,7 +23,7 @@ class TestApplicationConfiguration(unittest.TestCase):
     def test_socket_configuration(self):
         port = self.config.get(MusicServer._config_section, 'port')
         self.assertTrue(port.isdigit(), "Port should be a number, port = " + str(port))
-        self.assertTrue(len(str(port)) == 4, "Port should be 4 numbers long")
+        self.assertTrue(0 < int(port) < 65535, "Port should be a valid port number")
 
 
 if __name__ == '__main__':
